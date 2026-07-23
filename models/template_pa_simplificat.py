5a1620ff7b9bed60223a066fe8b733650dda3756
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-23 00:03:32 -0500

    Fix: Remove zero padding from accounts by setting code_digits to 0

--- models/template_pa_simplificat.py
+++ models/template_pa_simplificat.py
@@ -13,7 +13,7 @@ class AccountChartTemplate(models.AbstractModel):
             'property_account_payable_id': 'pa_account_220_02',
             'property_account_income_categ_id': 'pa_account_400_01',
             'property_account_expense_categ_id': 'pa_account_500_02',
-            'code_digits': 8,
+            'code_digits': 0,
         }
 
     @template('pa_simplificat', 'res.company')
@@ -21,9 +21,9 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.pa',
-                'bank_account_code_prefix': '100',
-                'cash_account_code_prefix': '100',
-                'transfer_account_code_prefix': '100',
+                'bank_account_code_prefix': '100.',
+                'cash_account_code_prefix': '100.',
+                'transfer_account_code_prefix': '100.9',
                 'account_default_pos_receivable_account_id': 'pa_account_120_01',
                 'income_currency_exchange_account_id': 'pa_account_601_01',
                 'expense_currency_exchange_account_id': 'pa_account_601_01',
